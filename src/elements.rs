d4f041bb14212a678e4ade0e4970940dcb349541
Katlyn Thomas <katlyn.c.thomas@gmail.com>
2023-06-18 16:53:45 -0500

    macro-tize me, captain!

--- src/elements.rs
+++ src/elements.rs
@@ -19,82 +19,147 @@ pub trait AddContent{
     fn add_content<C: ToHTML + 'static>(self, child: C) -> Self;
 }
 
-pub trait ElementTag {
-    fn as_tag(&self) -> String;
-}
-
-pub enum ElementType {
-    Formatting(Formatting),
-}
-
-impl ElementTag for ElementType {
-    fn as_tag(&self) -> String {
-        match self {
-            Self::Formatting(f) => f.as_tag(),
+macro_rules! impl_render_attribute {
+    ($($element:ident),+) => {
+        $(
+        pub struct $element {
+            pub tag: String,
+            pub attributes: HashMap<String, String>,
         }
-    }
-}
 
-pub enum Formatting {
-    Abbr,
-    Address,
-    B,
-    Bdi,
-    BlockQuote,
-}
-
-impl ElementTag for Formatting {
-    fn as_tag(&self) -> String {
-        use Formatting::*;
-        match self {
-            Abbr => "abbr".to_string(),
-            Address => "address".to_string(),
-            B => "b".to_string(),
-            Bdi => "bdi".to_string(),
-            BlockQuote => "blockquote".to_string(),
+        impl $element {
+            pub fn new() -> Self {
+                Self {
+                    tag: stringify!($element).to_lowercase(),
+                    attributes: HashMap::new(),
+                }
+            }
         }
-    }
-}
+        
+        impl ToHTML for $element {
+            fn render(&self) -> String {
+                let attributes = self.attributes.iter()
+                    .map(|(attr, val)|
+                        if attr == val { attr.clone() } 
+                        else { format!("{}=\"{}\"", attr, val) })
+                    .collect::<Vec<String>>()
+                    .join(" ");
 
-pub struct FormattingElement {
-    pub element: Formatting,
-    pub attributes: HashMap<String, String>,
-    pub content: Vec<Box<dyn ToHTML>>,
-}
-
-impl ToHTML for FormattingElement {
-    fn render(&self) -> String {
-        let attributes = self.attributes.iter()
-            .map(|(attr, val)|
-                if attr == val { attr.clone() } 
-                else { format!("{}=\"{}\"", attr, val) })
-            .collect::<Vec<String>>()
-            .join(" ");
-
-        let content = self.content.iter()
-            .map(|child| child.render())
-            .collect::<Vec<String>>()
-            .join("");
-
-        let tag = self.element.as_tag();
-        format!("<{} {}>{}</{}>", tag, attributes, content, tag)
-    }
-}
-
-impl AddAttribute for FormattingElement {
-    fn add_attribute(mut self, attribute: String, value: String) -> Self {
-        if let Some(attr) = self.attributes.get_mut(&attribute) {
-            attr.push_str(format!(" {}", value).as_str());
-        } else {
-            self.attributes.insert(attribute, value);
+                format!("<{} {} />", self.tag, attributes)
+            }
         }
-        self
-    }
+
+        impl AddAttribute for $element {
+            fn add_attribute(mut self, attribute: String, value: String) -> Self {
+                if let Some(attr) = self.attributes.get_mut(&attribute) {
+                    attr.push_str(format!(" {}", value).as_str());
+                } else {
+                    self.attributes.insert(attribute, value);
+                }
+                self
+            }
+        }
+        )+
+    };
 }
 
-impl AddContent for FormattingElement {
-    fn add_content<C: ToHTML + 'static>(mut self, child: C) -> Self {
-        self.content.push(Box::new(child));
-        self
-    }
+macro_rules! impl_render_attribute_content {
+    ($($element:ident),+) => {
+        $(
+        pub struct $element {
+            pub tag: String,
+            pub attributes: HashMap<String, String>,
+            pub content: Vec<Box<dyn ToHTML>>,
+        }
+
+        impl $element {
+            pub fn new() -> Self {
+                Self {
+                    tag: stringify!($element).to_lowercase(),
+                    attributes: HashMap::new(),
+                    content: Vec::new(),
+                }
+            }
+        }
+        
+        impl ToHTML for $element {
+            fn render(&self) -> String {
+                let attributes = self.attributes.iter()
+                    .map(|(attr, val)|
+                        if attr == val { attr.clone() } 
+                        else { format!("{}=\"{}\"", attr, val) })
+                    .collect::<Vec<String>>()
+                    .join(" ");
+
+                let content = self.content.iter()
+                    .map(|child| child.render())
+                    .collect::<Vec<String>>()
+                    .join("");
+
+                format!("<{} {}>{}</{}>", self.tag, attributes, content, self.tag)
+            }
+        }
+
+        impl AddAttribute for $element {
+            fn add_attribute(mut self, attribute: String, value: String) -> Self {
+                if let Some(attr) = self.attributes.get_mut(&attribute) {
+                    attr.push_str(format!(" {}", value).as_str());
+                } else {
+                    self.attributes.insert(attribute, value);
+                }
+                self
+            }
+        }
+
+        impl AddContent for $element {
+            fn add_content<C: ToHTML + 'static>(mut self, child: C) -> Self {
+                self.content.push(Box::new(child));
+                self
+            }
+        }
+        )+
+    };
 }
+
+// basic HTML tags
+impl_render_attribute_content!
+    (HTML, Head, Body, Style, Title);
+impl_render_attribute!
+    (Link, Meta);
+
+// content sectioning
+impl_render_attribute_content!(Address, Article, Aside,
+    Footer, Header, HGroup, Main, Nav, Section);
+impl_render_attribute_content!(H1, H2, H3, H4, H5, H6);
+
+// text content
+impl_render_attribute_content!(BlockQuote, DD, Div, DL, DT,
+    Figure, FigCaption, HR, LI, Menu, P, Pre, UL);
+
+// inline text semantics
+impl_render_attribute_content!(A, Abbr, B, BDI, BDO, BR, Cite, Code, Data, Del, DFN, Em, I, Ins,
+    KBD, Mark, Q, S, Samp, Small, Span, Strong, Sub, Sup, Time, U, Var, WBR);
+
+// multimedia
+impl_render_attribute_content!(Audio, Map, Video);
+impl_render_attribute!(Area, Img, Track);
+
+// embedded content
+impl_render_attribute_content!(IFrame, Object, Picture);
+impl_render_attribute!(Embed, Source);
+
+// scripting
+impl_render_attribute_content!(Canvas, NoScript, Script);
+
+// table contents
+impl_render_attribute_content!(Caption, Col, ColGroup, Table, TBody, TD, TFoot, TH, THead, TR);
+
+// forms
+impl_render_attribute_content!(Button, DataList, FieldSet, Form, Input, Label, Legend, Meter,
+    OptGroup, Option, Output, Progress, Select, TextArea);
+
+// interactive elements
+impl_render_attribute_content!(Details, Dialog, Summary);
+
+// TODO: WebComponents
+
