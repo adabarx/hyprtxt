3a01998698826a588302dda8142470fc890bd4fa
Katlyn Thomas <katlyn.c.thomas@gmail.com>
2023-06-18 01:46:04 -0500

    second pasS?!?!

--- src/elements.rs
+++ src/elements.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 use std::collections::HashMap;
 
 pub trait ToHTML {
@@ -10,101 +11,90 @@ impl ToHTML for String {
     }
 }
 
-pub trait BuildElement {
+pub trait AddAttribute {
     fn add_attribute(self, attribute: String, value: String) -> Self;
-    fn add_tag(self, tag: String) -> Self;
-    fn add_child<C: ToHTML + 'static>(self, child: C) -> Self;
 }
 
-macro_rules! impl_render {
-    ($($elem:ident),*) => {
-        $(
-            #[derive(Default)]
-            pub struct $elem {
-                children: Vec<Box<dyn ToHTML>>,
-                attributes: HashMap<String, String>,
-                tags: Vec<String>
-            }
-
-            impl ToHTML for $elem {
-                fn render(&self) -> String {
-                    let tags = self.tags.join(" ");
-
-                    let attributes = self.attributes.iter()
-                        .map(|(attr, val)| format!("{}=\"{}\"", attr, val))
-                        .collect::<Vec<String>>()
-                        .join(" ");
-
-                    let children = self.children.iter()
-                        .map(|child| child.render())
-                        .collect::<Vec<String>>()
-                        .join("");
-
-                    format!("<div {} {}>{}</div>", attributes, tags, children)
-                }
-            }
-
-            impl BuildElement for $elem {
-                fn add_attribute(mut self, attribute: String, value: String) -> Self {
-                    if let Some(attr) = self.attributes.get_mut(&attribute) {
-                        attr.push_str(format!(" {}", value).as_str());
-                    } else {
-                        self.attributes.insert(attribute, value);
-                    }
-                    self
-                }
-
-                fn add_tag(mut self, tag: String) -> Self {
-                    self.tags.push(tag);
-                    self
-                }
-
-                fn add_child<C: ToHTML + 'static>(mut self, child: C) -> Self {
-                    self.children.push(Box::new(child));
-                    self
-                }
-            }
-        )*
-    };
+pub trait AddContent{
+    fn add_content<C: ToHTML + 'static>(self, child: C) -> Self;
 }
 
-impl_render!(Div, Paragraph,);
+pub trait ElementTag {
+    fn as_tag(&self) -> String;
+}
 
-// impl ToHTML for Div {
-//     fn render(&self) -> String {
-//         let tags = self.tags.join(" ");
-//
-//         let attributes = self.attributes.iter()
-//             .map(|(attr, val)| format!("{}=\"{}\"", attr, val))
-//             .collect::<Vec<String>>()
-//             .join(" ");
-//
-//         let children = self.children.iter()
-//             .map(|&child| child.render())
-//             .collect::<Vec<String>>()
-//             .join("");
-//
-//         format!("<div {} {}>{}</div>", attributes, tags, children)
-//     }
-// }
-//
-// impl BuildElement for Div {
-//     fn add_attribute(mut self, attribute: String, value: String) -> Self {
-//         if let Some(attr) = self.attributes.get_mut(&attribute) {
-//             attr.push_str(format!(" {}", value).as_str());
-//         } else {
-//             self.attributes.insert(attribute, value);
-//         }
-//         self
-//     }
-//
-//     fn add_tag(mut self, tag: String) -> Self {
-//         self.tags.push(tag);
-//         self
-//     }
-//
-//     fn add_child<C: ToHTML>(mut self, child: C) -> Self {
-//         self.children.push(Box::new(child));
-//         self
-//     }
-// }
+pub enum ElementType {
+    Formatting(Formatting),
+}
+
+impl ElementTag for ElementType {
+    fn as_tag(&self) -> String {
+        match self {
+            Self::Formatting(f) => f.as_tag(),
+        }
+    }
+}
+
+pub enum Formatting {
+    Abbr,
+    Address,
+    B,
+    Bdi,
+    BlockQuote,
+}
+
+impl ElementTag for Formatting {
+    fn as_tag(&self) -> String {
+        use Formatting::*;
+        match self {
+            Abbr => "abbr".to_string(),
+            Address => "address".to_string(),
+            B => "b".to_string(),
+            Bdi => "bdi".to_string(),
+            BlockQuote => "blockquote".to_string(),
+        }
+    }
+}
+
+pub struct FormattingElement {
+    pub element: Formatting,
+    pub attributes: HashMap<String, String>,
+    pub content: Vec<Box<dyn ToHTML>>,
+}
+
+impl ToHTML for FormattingElement {
+    fn render(&self) -> String {
+        let attributes = self.attributes.iter()
+            .map(|(attr, val)|
+                if attr == val { attr.clone() } 
+                else { format!("{}=\"{}\"", attr, val) })
+            .collect::<Vec<String>>()
+            .join(" ");
+
+        let content = self.content.iter()
+            .map(|child| child.render())
+            .collect::<Vec<String>>()
+            .join("");
+
+        let tag = self.element.as_tag();
+        format!("<{} {}>{}</{}>", tag, attributes, content, tag)
+    }
+}
+
+impl AddAttribute for FormattingElement {
+    fn add_attribute(mut self, attribute: String, value: String) -> Self {
+        if let Some(attr) = self.attributes.get_mut(&attribute) {
+            attr.push_str(format!(" {}", value).as_str());
+        } else {
+            self.attributes.insert(attribute, value);
+        }
+        self
+    }
+}
+
+impl AddContent for FormattingElement {
+    fn add_content<C: ToHTML + 'static>(mut self, child: C) -> Self {
+        self.content.push(Box::new(child));
+        self
+    }
+}
