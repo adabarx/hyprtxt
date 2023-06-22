addc03c4b774ff57d2cf18485a1e710f6571f615
katlyn-thomas <katlyn.c.thomas@gmail.com>
2023-06-21 22:52:20 -0500

    bund-lar

--- ssg/src/lib.rs
+++ ssg/src/lib.rs
@@ -1,4 +1,6 @@
+#![allow(unused)]
 use std::collections::HashMap;
+use std::{io::Error, fs};
 
 use hyprtxt::hyprtxt;
 
@@ -74,17 +76,52 @@ impl From<&str> for Endpoint {
 }
 
 pub trait StaticSite {
+    fn bundle_assets(&mut self) -> Result<Vec<String>, std::io::Error>;
     fn add_endpoint(&mut self, path: Endpoint, response: String);
     fn generate(&self) -> Result<(), std::io::Error>;
 }
 
 impl StaticSite for HashMap<Endpoint, String> {
+    fn bundle_assets(&mut self) -> Result<Vec<String>, Error> {
+        let paths = fs::read_dir("./assets")?;
+        let mut rv = vec![];
+        for path in paths {
+            if let Ok(p) = path {
+                let file_name = p.path()
+                    .file_name()
+                    .unwrap()
+                    .to_string_lossy()
+                    .to_string();
+
+                if let Some(ext) = file_name.split(".").last() {
+                    match ext {
+                        "js" => {
+                            let path = "./public/assets/js";
+                            let to = [path, file_name.as_ref()].join("");
+                            fs::create_dir_all(path)?;
+                            fs::copy(p.path(), &to)?;
+                            rv.push(to);
+                        },
+                        "css" => {
+                            let path = "./public/assets/css";
+                            let to = [path, file_name.as_ref()].join("");
+                            fs::create_dir_all(path)?;
+                            fs::copy(p.path(), &to)?;
+                            rv.push(to);
+                        },
+                        _ => continue,
+                    }
+                }
+            }
+        }
+        Ok(rv)
+    }
+
     fn add_endpoint(&mut self, path: Endpoint, response: String) {
         self.insert(path, response);
     }
 
-    fn generate(&self) -> Result<(), std::io::Error> {
-        use std::fs;
+    fn generate(&self) -> Result<(), Error> {
         let prefix = "public";
 
         for (endpoint, page) in self.iter() {
