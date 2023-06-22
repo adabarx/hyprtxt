046837a5bbe990617f4fac04e80860c9b6378fb2
katlyn-thomas <katlyn.c.thomas@gmail.com>
2023-06-22 18:12:51 -0500

    new proc_macro syntax

--- txt/src/lib.rs
+++ txt/src/lib.rs
@@ -1,8 +1,10 @@
+#![allow(unused)]
 use proc_macro::TokenStream;
 use quote::{quote, ToTokens, TokenStreamExt};
 use syn::{
-    parse::{ Parse, ParseStream, ParseBuffer }, 
-    braced, Ident, token, Token, Expr
+    parse::{ Parse, ParseStream, ParseBuffer, Parser },
+    braced, Ident, token, Token, Expr,
+    ext::IdentExt,
 };
 
 
@@ -40,85 +42,158 @@ impl ToTokens for ContentStream {
 }
 
 #[derive(Debug)]
-enum AttrStream {
-    KeyVal(Ident, Expr),
-    Val(Ident)
-}
+struct AttrStream(Ident, Expr);
 
 impl Parse for AttrStream {
-    fn parse(input: ParseStream) -> syn::Result<Self> {
-        if let Ok(ident) = input.parse::<Ident>() {
-            if input.peek(Token![:]) {
-                let _: Token![:] = input.parse()?;
-                let lit: Expr = input.parse()?;
-                Ok(Self::KeyVal(ident, lit))
-            } else {
-                Ok(Self::Val(ident))
-            }
-        } else {
-            Err(input.error("invalid attr"))
+    fn parse(stream: ParseStream) -> syn::Result<Self> {
+        if stream.peek(Token!(.)) {
+            let _: Token!(.) = stream.parse()?;
+            let _: Token!(=) = stream.parse()?;
+            return Ok(Self(Ident::parse_any.parse_str("class")?, stream.parse::<Expr>()?))
         }
+
+        if stream.peek(Token!(#)) {
+            let _: Token!(#) = stream.parse()?;
+            let _: Token!(=) = stream.parse()?;
+            return Ok(Self(Ident::parse_any.parse_str("id")?, stream.parse::<Expr>()?))
+        }
+
+        let ident = stream.call(Ident::parse_any)?;
+        let _: Token!(=) = stream.parse()?;
+        let value: Expr = stream.parse()?;
+
+        Ok(Self(ident, value))
     }
 }
 
 impl ToTokens for AttrStream {
     fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
-        match self {
-            Self::KeyVal(k, v) => {
-                let k = k.to_string();
-                tokens.append_all(quote! { format!(" {}=\"{}\"", #k, #v) });
-            },
-            Self::Val(v) => {
-                let v = v.to_string();
-                tokens.append_all(quote! { format!(" {}", #v) })
-            },
-        }
+        let key = self.0.to_string();
+        let value = &self.1;
+        tokens.append_all(quote! { format!(" {}=\"{}\"", #key, #value) });
     }
 }
 
 struct ElementStream {
     tag: Ident,
+    void: bool,
     attrs: Vec<AttrStream>,
     content: Vec<Box<dyn ToTokens>>,
 }
 
 impl ElementStream {
-    pub fn new(tag: Ident) -> Self {
+    pub fn new(tag: Ident, void: bool) -> Self {
         Self {
             tag,
+            void,
             attrs: vec![],
             content: vec![]
         }
     }
 }
 
+enum ElemTypeResult {
+    Void,
+    Content,
+    Default,
+}
+
+impl ElemTypeResult {
+    pub fn new(s: &ParseBuffer) -> syn::Result<Self> {
+        if s.peek(token::Brace) {
+            return Ok(Self::Default)
+        };
+
+        if s.peek(Token!(*)) {
+            return Ok(Self::Void)
+        };
+
+        if s.peek(Token!(:)) {
+            return Ok(Self::Content)
+        }
+
+        Err(s.error("Invalid Element"))
+    }
+}
+
+enum PeekInsideElemResult {
+    Element,
+    Content,
+    Attribute,
+    Class,
+    Id,
+}
+
+impl PeekInsideElemResult {
+    pub fn new(s: &ParseBuffer) -> syn::Result<Self> {
+        // p {} -> <p></p>
+        if s.peek(Ident::peek_any) && s.peek2(token::Brace) {
+            return Ok(Self::Element)
+        };
+        // p: "x" -> <p>x</p>
+        if s.peek(Ident::peek_any) && s.peek2(Token!(:)) {
+            return Ok(Self::Element)
+        };
+        // { $: "x" } -> <>x</>
+        if s.peek(Token!($)) && s.peek2(Token!(:)) {
+            return Ok(Self::Content)
+        };
+        // p { class="stuff" } -> <p class="stuff"></p>
+        if s.peek(Ident::peek_any) && s.peek2(Token!(=)) {
+            return Ok(Self::Attribute)
+        };
+        // p { .="stuff" } -> <p class="stuff"></p>
+        if s.peek(Token!(.)) && s.peek2(Token!(=)) {
+            return Ok(Self::Class)
+        };
+        // p { #="stuff" } -> <p id="stuff"></p>
+        if s.peek(Token!(#)) && s.peek2(Token!(=)) {
+            return Ok(Self::Id);
+        };
+        if s.peek(Ident::peek_any) && s.peek2(Token!(*)) && s.peek3(token::Brace) {
+            return Ok(Self::Element)
+        };
+        Err(s.error("Invalid Syntax"))
+    }
+}
+
+
 impl Parse for ElementStream  {
     fn parse(stream: ParseStream) -> syn::Result<Self> {
-        if let Ok(ident) = stream.parse::<Ident>() {
-            let braced_stream: ParseBuffer;
-            let _ = braced!(braced_stream in stream);
+        let ident = stream.call(Ident::parse_any)?;
 
-            let mut element = ElementStream::new(ident);
+        let mut element = match ElemTypeResult::new(stream)? {
+            ElemTypeResult::Default => ElementStream::new(ident, false),
+            ElemTypeResult::Void => ElementStream::new(ident, true),
+            ElemTypeResult::Content => {
+                let _: Token!(:) = stream.parse()?;
+                let mut element = ElementStream::new(ident, false);
+                element.content.push(Box::new(stream.parse::<ContentStream>()?));
+                return Ok(element)
+            },
 
-            while !braced_stream.is_empty() {
-                // is content, attr, or error
-                if braced_stream.peek(Ident) && braced_stream.peek2(token::Brace) {
-                    element.content.push(Box::new(braced_stream.parse::<ElementStream>()?));
-                } else if braced_stream.peek(Token!($)) && braced_stream.peek2(Token!(:)) {
-                    element.content.push(Box::new(braced_stream.parse::<ContentStream>()?));
-                } else if braced_stream.peek(Ident) && braced_stream.peek2(Token![:]) {
-                    element.attrs.push(braced_stream.parse()?);
-                } else if braced_stream.peek(Token![,]) {
-                    let _: Token![,] = braced_stream.parse()?;
-                } else {
-                    return Err(braced_stream.error("neither attr nor content"))
-                }
-            };
+        };
+        
+        let b_stream: ParseBuffer;
+        let _ = braced!(b_stream in stream);
 
-            Ok(element)
-        } else {
-            Err(stream.error("invalid element"))
-        }
+        while !b_stream.is_empty() {
+            match PeekInsideElemResult::new(&b_stream)? {
+                PeekInsideElemResult::Content =>
+                    element.content.push(Box::new(b_stream.parse::<ContentStream>()?)),
+                PeekInsideElemResult::Element =>
+                    element.content.push(Box::new(b_stream.parse::<ElementStream>()?)),
+                PeekInsideElemResult::Id => {
+                    let _: Token!(.) = b_stream.parse()?;
+                    let _: Token!(=) = b_stream.parse()?;
+                    // element.attrs.push(value)
+                },
+                PeekInsideElemResult::Class => todo!(),
+                PeekInsideElemResult::Attribute => todo!(),
+            }
+        };
+
+        Ok(element)
     }
 }
 
